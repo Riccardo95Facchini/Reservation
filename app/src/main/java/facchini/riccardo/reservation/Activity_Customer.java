0ba0fc8281fd43fdb6a31e7d7cd74dbcffe772df
Riccardo Facchini <riccardo95facchini@gmail.com>
2019-02-11 17:52:38 +0100

    Changed colors

--- app/src/main/java/facchini/riccardo/reservation/Activity_Customer.java
+++ app/src/main/java/facchini/riccardo/reservation/Activity_Customer.java
@@ -1,6 +1,5 @@
 package facchini.riccardo.reservation;
 
-import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
@@ -11,27 +10,21 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.Toast;
 
 import com.firebase.ui.auth.AuthUI;
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.GoogleApiAvailability;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class Activity_Customer extends AppCompatActivity
 {
-    private static final String TAG = "ECCEZIONE";
     private FirebaseAuth.AuthStateListener authStateListener;
     
     private byte backButton;
@@ -214,9 +207,7 @@ public class Activity_Customer extends AppCompatActivity
         FirebaseAuth.getInstance().addAuthStateListener(authStateListener);
         
         if (currentMenu == R.id.bottomSearch && isMapsEnabled())
-        {
             getLocationPermission();
-        }
     }
     
     private void setupFirebaseListener()
